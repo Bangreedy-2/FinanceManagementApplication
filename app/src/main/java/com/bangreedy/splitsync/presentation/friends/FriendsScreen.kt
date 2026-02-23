158d90e533ef96bb7cc354662418e7cf35ae82fb
Bangreedy-2 <andreicliucovschi@gmail.com>
2026-02-23 19:17:12 +0200

    feat: Add Account, Activity, and Friends screens with navigation

---
+++ app/src/main/java/com/bangreedy/splitsync/presentation/friends/FriendsScreen.kt
@@ -0,0 +1,16 @@
+package com.bangreedy.splitsync.presentation.friends
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FriendsScreen() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("Friends Screen - Coming Soon")
+    }
+}
+
